5909531e3c50d491ffeadd0becac85953b8b3a84
siffy-maker <153082439+siffy-maker@users.noreply.github.com>
2023-12-06 11:15:33 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+    basic.showString("Hello!")
+})
